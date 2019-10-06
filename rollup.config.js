d65223cf66b37005a51b0664383cbb04386274a2
Robin <robin@robin-it.com>
2019-10-06 13:03:49 +0200

    Use rollup to build a browser compatible UMD package.

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+/* global process require */
+
+import node from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    format: 'umd',
+    name: 'SourcecredPayouts',
+    file: 'index.js'
+  },
+  plugins: [
+    node(),
+    commonjs()
+  ],
+};
