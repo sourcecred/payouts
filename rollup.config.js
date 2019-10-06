07ba93af66271a24c179fd8de85847deb373c62f
Robin <robin@robin-it.com>
2019-10-06 17:49:34 +0200

    Pretty rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,14 @@
 /* global process require */
 
-import node from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import node from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    format: 'umd',
-    name: 'SourcecredPayouts',
-    file: 'index.js'
+    format: "umd",
+    name: "SourcecredPayouts",
+    file: "index.js",
   },
-  plugins: [
-    node(),
-    commonjs()
-  ],
+  plugins: [node(), commonjs()],
 };
